51a388d5f0438242f1dd773fb07cea2fbbc3b166
shin donggyu <donggyu9410@gmail.com>
2019-09-15 19:35:34 +0900

    idk

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import graphqlHttp from 'express-graphql';
 import schema from './schema/schema'
 import expressPlayground from 'graphql-playground-middleware-express'
+import cors from 'cors'
 import './mongoose/mongoose'
 import './models'
 
 const app: express.Application = express();
 const PORT: number = 4000;
+
+// allow cross-origin requests
+app.use(cors())
 
 app.use('/graphql', graphqlHttp({
     schema,
